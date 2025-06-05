cf983f0c22085ec5c8f80723a8c5f5513eafada6
Pruthviacharyaj <pruthviacharya2484@gmail.com>
2025-06-05 08:04:40 +0530

    initial commit

---
+++ src/main/java/exam/pru/App.java
@@ -0,0 +1,13 @@
+package exam.pru;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
